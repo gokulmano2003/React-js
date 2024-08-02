495acc2d1fcaceada50df2cca2a3e51acc954796
gokulmano2003 <121616386+gokulmano2003@users.noreply.github.com>
2024-08-02 14:51:47 +0530

    Create project.js

---
+++ student/pages/project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => (
+  <div>
+    <h2>My Projects</h2>
+    <p>This is the Projects page.</p>
+    <p>more then Projects page's.</p>
+  </div>
+);
+
+export default Projects;
