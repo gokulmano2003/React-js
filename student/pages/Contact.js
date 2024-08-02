56faab96fca0476b992238cc3fb6188764befae2
gokulmano2003 <121616386+gokulmano2003@users.noreply.github.com>
2024-08-02 14:50:43 +0530

    Create Contact.js

---
+++ student/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <h2>Contact Me</h2>
+    <p>This is the Contact page.</p>
+    <p> Address,Contact Number,Email And location</p>
+  </div>
+);
+
+export default Contact;
