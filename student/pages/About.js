bfa4f0cd53cfdad5bd25f6d9fc4ae0f93b14d2ac
gokulmano2003 <121616386+gokulmano2003@users.noreply.github.com>
2024-08-02 14:50:11 +0530

    Create About.js

---
+++ student/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h2>About Me</h2>
+    <p>This is the About Section..</p>
+    <p>See then more Content's</p>
+  </div>
+);
+
+export default About;
